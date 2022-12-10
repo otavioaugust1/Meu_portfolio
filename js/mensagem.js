12a23827cdf17abe3cbc5937485e611e8b337b1a
Otavio Augusto <otavioaugust@gmail.com>
2022-12-10 14:03:20 -0300

    version 2.15

--- js/mensagem.js
+++
@@ -1,9 +0,0 @@
-function Comentarios() {
-    if ($('.product-tabs').length != 0) {
-        $('a[href=\'#tab-review\']').trigger('click');
-        $('#input-name').focus();  // adiciona foco ao primeiro input do form de comentarios
-        $("html, body").animate({
-            scrollTop: $('.product-tabs').offset().top
-        }, 1000);
-    }
-}
